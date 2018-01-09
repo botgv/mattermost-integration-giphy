736238d370206d7592232ce6f82b42d2aa5896a9
Florent VIELLE <florent.vielle@fr.office.servicemagic.eu>
2018-01-09 18:49:56 +0100

    Add giphy proxy

--- mattermost_giphy/app.py
+++ mattermost_giphy/app.py
@@ -88,6 +88,7 @@ def new_post():
     finally:
         resp = Response(content_type='application/json')
         resp.set_data(json.dumps(resp_data))
+		app.logger.info(resp_data['text'])
         return resp
 
 
