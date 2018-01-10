0dacf3358a4a5760c517c55c3e6aa9613ceaa821
Florent VIELLE <florent.vielle@fr.office.servicemagic.eu>
2018-01-10 09:32:13 +0100

    Add giphy proxy

--- mattermost_giphy/app.py
+++ mattermost_giphy/app.py
@@ -79,8 +79,8 @@ def new_post():
         if not gif_url:
             raise Exception('No gif url found for `{}`'.format(translate_text))
 
-        resp_data['text'] = '''`{}` searched for {}
-    {}redirect/{}'''.format(data.get('user_name', 'unknown').title(), translate_text, request.host_url, base64.encodestring(gif_url))
+        resp_data['text'] = "`{}` searched for {}
+    {}redirect/{}.gif".format(data.get('user_name', 'unknown').title(), translate_text, request.host_url, base64.encodestring(gif_url))
     except Exception as err:
         msg = err.message
         logging.error('unable to handle new post :: {}'.format(msg))
