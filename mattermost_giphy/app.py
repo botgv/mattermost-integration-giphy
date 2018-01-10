2d0621c95eb84a084ff5ba94e544eb11a564db52
Florent VIELLE <florent.vielle@fr.office.servicemagic.eu>
2018-01-10 12:25:40 +0100

    Add giphy proxy

--- mattermost_giphy/app.py
+++ mattermost_giphy/app.py
@@ -56,11 +56,10 @@ def new_post():
         resp_data['username'] = USERNAME
         resp_data['icon_url'] = ICON_URL
 
-        data['text'] = request.form
-        app.logger.info(request.form)
-        #channel = data['channel_name']
-        #if channel not in BLACK_LISTED_CHANNELS:
-        #    raise Exception('Gif not allowed in channel `{}`'.format(channel))
+        data = request.form
+        channel = data['channel_name']
+        if channel not in BLACK_LISTED_CHANNELS:
+            raise Exception('Gif not allowed in channel `{}`'.format(channel))
 
         if not 'token' in data:
             raise Exception('Missing necessary token in the post data')
